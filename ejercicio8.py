638d03f33f708a6206bc3bb52181d1df3b04c538
Juan David Granados Diaz <144927336+Jgranaddavid@users.noreply.github.com>
2023-09-18 10:48:41 -0500

    Add files via upload

---
+++ ejercicio8.py
@@ -0,0 +1,43 @@
+#los ciclos para la programacion son secuenciales que permitenb la realizacion o ejecucion de un codigo
+#funciones para poder abreviar o automatizar un codigo
+#ciclo while que significa mientras que
+
+""
+#print("Tipo de programa que incluya un ciclo")
+
+#print("--------------Inio del ciclo----------------------")
+
+#for i in range (10):
+    #print(i**4)
+
+#print("Finalize el ciclo")
+
+
+#El programa que vamos a realizar tiene como finalidad realiza un ciclo en donde le 
+#ingresar hasta un 3 un numero, en este caso la edad, y cuando genere la variable correcta
+#ejecute en pantalla la siguiente accion: digita tu numero de cedula
+#saber el numero de silla donde fue asignado
+
+print("Admision para entrar")
+
+nombre=str(input("Ingrese su nombre"))
+edad=int(input("Ingrese su edad"))
+Identificacion=int(input("Ingrese su identificacion"))
+if edad >= 18:
+     edad>=18 and edad<=25
+     ubicacion="Zona A"
+     ZonaA=["1","2","3","4","5"]
+     print(ZonaA)
+elif edad>=26 and edad<=34:
+     ubicacion="Zona B"
+     edad
+     print("correcta")
+else:
+      ubicacion="Zona C" 
+      edad<18
+      print("edad incorrecta") 
+datos=(nombre,edad,Identificacion,ubicacion)
+
+#edad=(18<1)
+#print("Edad incorrecta")
+print(datos)
