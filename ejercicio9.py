b3cab063f5548773f2372b95b2d583b9acec261a
Juan David Granados Diaz <144927336+Jgranaddavid@users.noreply.github.com>
2023-09-22 10:44:23 -0500

    Add files via upload

---
+++ ejercicio9.py
@@ -0,0 +1,42 @@
+#Vamosa realizar unn programa que realice una tupla compuesta
+#por diferentes tipos de datos que incluyen variables
+#adicionalmente que esten orientados a realizar diferentes acciones
+#enfocadas en generar resultados en variaciones
+
+#print("Este es un programa de tuplas")
+
+#n=[(1,4),(4,8)]
+#t=[("Juan David",1),(34,"Hogar")]
+
+#print(n,t)
+
+#res=()
+
+#for n in res:
+    #print(n)
+    #if n==res:
+        #print(res)
+
+#Localizar dentro de una tuopla la posicion determinada por un operador de referencia como el :        
+
+
+
+#A=("casa","programa para posicionar", "tuplas en una variable", [2,9,2022])
+
+#print(A[2:3])
+
+#for A in True:
+    #print(A)
+    #if A==True:
+        #print("casa")
+
+
+A=("casa","carro","avion","celular","1","2","3","4","5",[2,9,2002],"gato","amor","cargador")
+
+print(A[0:1])
+print(A[2:4])
+print(A[3:9])
+
+
+
+
